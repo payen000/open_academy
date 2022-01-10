7c0cce4255344136b1cce5c34b488c266147af50
payen000 <49595803+payen000@users.noreply.github.com>
2022-01-09 18:29:03 -0700

    [ADD] open_academy: Module Creation T#53764

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "open_academy",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'license': 'LGPL-3',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
