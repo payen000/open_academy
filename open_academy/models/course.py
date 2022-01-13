caba043f121a00bedd65cf43ffe7a7b250271074
payen000 <payen@vauxoo.com>
2022-01-13 11:12:14 -0700

    [IMP] open_academy: Added one2many that relates each course to their sessions T#53764

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -8,3 +8,4 @@ class Course(models.Model):
     title = fields.Char(required=True)
     description = fields.Text()
     responsible_user_id = fields.Many2one('res.users')
+    session_ids = fields.One2many('session', 'course_id')
