3642efcf463a3c3270c340f581f97e61aaf6d298
payen000 <payen@vauxoo.com>
2022-01-14 14:42:00 -0700

    [IMP] open_academy: Added explicit onchange to warn about invalid number of seats or attendees in Session model T#53764
    If the number of seats in a session is less than the number of attendees, the following happens:
    
        if the number of seats is now less than 0, set it back to its old value and trigger two warnings: one for less than 0 seats and another for maximum capacity exceeded;
        if the number of seats is now not less than 0, set the attendee_ids back to its old value and trigger a warning for maximum capacity exceeded.

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -20,3 +20,23 @@ class Session(models.Model):
         for record in self:
             taken_seats_percentage = int(100*len(record.attendee_ids)/record.number_of_seats) if record.number_of_seats > 0 else 0
             record.taken_seats_percentage = taken_seats_percentage
+
+    @api.onchange('attendee_ids', 'number_of_seats')
+    def _onchange_seats(self):
+        warning_message = [] 
+
+        if self.number_of_seats < len(self.attendee_ids):
+            if self.number_of_seats < 0:
+                warning_message.append('There cannot be less than 0 seats available.')
+
+            self.number_of_seats = self._origin.number_of_seats
+            self.attendee_ids = self._origin.attendee_ids
+            warning_message.append('Maximum seat capacity exceeded; discarded operation.')
+
+        if warning_message:
+            return {
+                'warning': {
+                    'title': 'Error in number of seats or attendees',
+                    'message': ' '.join(warning_message),
+                }
+            }
