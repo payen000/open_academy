09ba909ddf13a0fa054289d36ec2533ec1b8002e
payen000 <payen@vauxoo.com>
2022-01-14 12:58:55 -0700

    [IMP] open_academy: Added percentage of taken seats to Session model and displayed it in its tree and form views as a progress bar T#53764

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Session(models.Model):
@@ -12,3 +12,10 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', domain="['|',('is_instructor', '=', True),('category_id.name', 'like', 'Teacher')]")
     course_id = fields.Many2one('course', required=True)
     attendee_ids = fields.Many2many('res.partner')
+    taken_seats_percentage = fields.Char('Taken Seats %', compute='_compute_taken_seats_percentage')
+
+    @api.depends('attendee_ids', 'number_of_seats')
+    def _compute_taken_seats_percentage(self):
+        for record in self:
+            taken_seats_percentage = int(100*len(record.attendee_ids)/record.number_of_seats) if record.number_of_seats > 0 else 0
+            record.taken_seats_percentage = taken_seats_percentage
