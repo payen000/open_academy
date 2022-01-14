9aa180a971df90b23cb5894f4284e0948e2dc09b
payen000 <payen@vauxoo.com>
2022-01-14 12:36:25 -0700

    [IMP] open_academy: Added domain to Session to show only partners with is_instructor=True when selecting an instructor for a session T#53764

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -9,6 +9,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float()
     number_of_seats = fields.Integer()
-    instructor_id = fields.Many2one('res.partner')
+    instructor_id = fields.Many2one('res.partner', domain="[('is_instructor', '=', True)")
     course_id = fields.Many2one('course', required=True)
     attendee_ids = fields.Many2many('res.partner')
