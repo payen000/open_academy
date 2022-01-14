44e210f40ed2f689669b84301d76525db706562e
payen000 <payen@vauxoo.com>
2022-01-14 13:03:29 -0700

    [IMP] open_academy: In Session model: added start_date default value as today's date and added active field with default value True T#53764

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -6,7 +6,8 @@ class Session(models.Model):
     _description = 'Session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
+    active = fields.Boolean(default=True)
     duration = fields.Float()
     number_of_seats = fields.Integer()
     instructor_id = fields.Many2one('res.partner', domain="['|',('is_instructor', '=', True),('category_id.name', 'like', 'Teacher')]")
