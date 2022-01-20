dd178904fe06fb5c245b89b98ca1afbf25b87821
payen000 <payen@vauxoo.com>
2022-01-20 14:00:56 -0700

    [IMP] open_academy: Added buttons to update attendees to a given session from the wizard and a cancel button T#53764

--- open_academy/wizards/assign_attendee_sessions.py
+++ open_academy/wizards/assign_attendee_sessions.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, _
+from odoo.exceptions import ValidationError
 
 
 class AssignAttendeeSessions(models.TransientModel):
@@ -7,3 +8,9 @@ class AssignAttendeeSessions(models.TransientModel):
 
     session_id = fields.Many2one('session')
     attendee_ids = fields.Many2many('res.partner')
+
+    def action_register_attendees(self):
+        if self.session_id.number_of_seats < len(self.attendee_ids):
+            raise ValidationError(_('Aborted registration: attendee limit exceeded'))
+
+        self.session_id.attendee_ids = self.attendee_ids
