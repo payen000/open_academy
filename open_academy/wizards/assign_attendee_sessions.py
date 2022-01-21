f35878c331c92444ab88dc00741bcf38a8096128
payen000 <payen@vauxoo.com>
2022-01-20 17:02:14 -0700

    [IMP] open_academy: Modified assign_attendee_sessions wizard and its view so that multiple attendees can be registered to multiple sessions at once T#52897

--- open_academy/wizards/assign_attendee_sessions.py
+++ open_academy/wizards/assign_attendee_sessions.py
@@ -1,16 +1,21 @@
 from odoo import models, fields, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 
 class AssignAttendeeSessions(models.TransientModel):
     _name = 'assign.attendee.sessions'
     _description = 'Assign Attendee Sessions'
 
-    session_id = fields.Many2one('session')
+    session_ids = fields.Many2many('session')
     attendee_ids = fields.Many2many('res.partner')
 
     def action_register_attendees(self):
-        if self.session_id.number_of_seats < len(self.attendee_ids):
-            raise ValidationError(_('Aborted registration: attendee limit exceeded'))
+        overloaded_sessions = self.session_ids.filtered(
+            lambda session: session.number_of_seats < len(self.attendee_ids)
+        )
 
-        self.session_id.attendee_ids = self.attendee_ids
+        if overloaded_sessions:
+            overloaded_sessions = ', '.join([session.name for session in overloaded_sessions])
+            raise UserError(_('Aborted registration: attendee limit exceeded in %s.', overloaded_sessions))
+
+        self.session_ids.write({'attendee_ids': self.attendee_ids})
